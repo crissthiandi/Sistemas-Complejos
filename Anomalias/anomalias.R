27d093287a8457b73ee5a56d31c868235e4d3093
crissthiandi <albertocenaa@gmail.com>
2020-12-08 15:53:20 -0600

    imagenes para la documentación

--- Anomalias/anomalias.R
+++ Anomalias/anomalias.R
@@ -1,30 +1,35 @@
+#Bibliotecas a usar en R-software
 library(tidyverse)
+#Lectura de los datos zahn
 base=read.csv("C:/Users/Acer E15/Downloads/zahn.csv",sep = "\t",header = F)
 base=tibble(base)
 base
 
+#Vista
 ggplot(base,aes(x = V1,y = V2,color=V3))+
-  geom_point(show.legend = F)
+  geom_point(show.legend = F)+
+  ggtitle("Grafico que visualiza los datos")+
+  labs(x = "componente 1",y = "Componente 2")
 
-#diametro c:
+#distancias matrix
 distancia=dist(cbind(base$V1,base$V2),method = "euclidean",diag = T)
 distancia
-max(distancia) #diametro
-min(distancia)
-head(distancia,15)
+max(distancia) #diametro de los datos
+min(distancia) #distancia minima general
 
-#metodo manual
+#metodo manual para vecinos cercano
 
 #primer vecino v1()
 
-df <- reshape::melt(as.matrix(distancia), varnames = c("filas", "colum"))
-minimo=df %>%
+distancia_df <- reshape::melt(as.matrix(distancia), varnames = c("filas", "colum"))
+minimo=distancia_df %>%
   filter(colum!=filas) %>%
   group_by(colum) %>%
   summarise(vecino_cercano=min(value),cordenada=which.min(value)+1)
 minimo
-
-diametro=df %>%
+knitr::kable(head(minimo))
+#diametro
+diametro=distancia_df %>%
   filter(colum!=filas) %>%
   group_by(colum) %>%
   summarise(vecino_lejano=max(value))
@@ -32,13 +37,14 @@ diametro$vecino_lejano %>% max()
 
 direccion_=minimo$cordenada+399*(minimo$colum-1)
 direccion_
-minimo_2=df %>%
+minimo_2=distancia_df %>%
   mutate(fila=row_number()) %>%
   filter(colum!=filas & !fila %in% direccion_ ) %>%
   group_by(colum) %>%
   summarise(fila=which.min(value)+1,vecino_cercano_2=min(value))
 minimo_2
 
+knitr::kable(head(minimo_2))
 
 
 Ratio=minimo %>%
@@ -46,12 +52,19 @@ Ratio=minimo %>%
   mutate(ratio=vecino_cercano_2/vecino_cercano)
 Ratio
 
+knitr::kable(head(Ratio))
+
+
 #grafico de ratios
-ggplot(Ratio,aes(colum,cordenada,color=ratio))+
-  geom_point()
+ggplot(Ratio,aes(colum,cordenada,size=ratio,color=ratio))+
+  geom_point()#+geom_jitter(width = 10, height = 10)
+
+ggsave(filename = "por_cordenada_ruidizado.png" )
+ggsave(filename = "por_cordenada.png" )
 
 #buscador de anomalos
 ggplot(Ratio,aes(colum,ratio))+
   geom_point()+
   geom_text(aes(label=cordenada))
 
+ggsave(filename = "por_ratio.png" )
