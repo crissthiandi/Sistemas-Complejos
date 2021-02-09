e8d0497302acd400bd794227d8dd59e6ae5f03c9
crissthiandi <albertocenaa@gmail.com>
2021-02-09 11:48:11 -0600

    Script incompleto de el curso de analisis de redes en python para posteriores analisis

---
+++ network_taller.py
@@ -0,0 +1,14 @@
+import networkx as nx
+
+#red de erdos 
+g = nx.erdos
+
+#red de barabasi que es libre de escala
+h
+#red de mundo pequeño 
+i
+
+#con esto se puede jugar con ellas desde graficarlas hasta interactuarlas 
+
+
+#genera nodos y luego los añade
